a129a202ee8e0ee4aa50a151858d1c9baf62ad31
Vinicius.Araujo <vinicius.araujo@ifpi.edu.br>
2012-09-21 20:23:17 -0300

    adionados documentos para elabora estrategia

---
+++ Grupo Elicitare Projetos/Portifólio de Projetos em arvore binaria de prioridade  Grupo Elicitare.mm
@@ -0,0 +1,28 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1347628175226" ID="ID_329284621" MODIFIED="1347629357242" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Portif&#243;lio de Projetos
+    </p>
+    <p>
+      Grupo Elicitare
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1347628347523" ID="ID_1011734926" MODIFIED="1347629370013" POSITION="right" TEXT="P1:Ficar rico">
+<node CREATED="1347628406483" ID="ID_1115421591" MODIFIED="1347629349345" TEXT="P3: OBTER FLUENCIA EM INGLES">
+<node CREATED="1347629408607" ID="ID_189241902" MODIFIED="1347630029333" TEXT=""/>
+</node>
+<node CREATED="1347628416003" ID="ID_801154726" MODIFIED="1347630037303" TEXT="P27: Projeto de Investimentos">
+<node CREATED="1347629481624" ID="ID_1220837891" MODIFIED="1347629556478" TEXT="P26: Projeto compra da casa"/>
+<node CREATED="1347629562850" ID="ID_1921333196" MODIFIED="1347630025345" TEXT="P2: OBTER MESTRADO"/>
+</node>
+</node>
+</node>
+</map>
